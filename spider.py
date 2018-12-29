593c68fa5312b5e7397951132d02cd9362a4471e
mjdavy <mjdavy@hotmail.com>
2018-12-29 12:18:46 -0500

    added first unit test

--- spider.py
+++ spider.py
@@ -7,12 +7,15 @@ class Spider:
         http = urllib3.PoolManager()
         response = http.request('GET', targetUrl)
         if response.status == 200:
-            self.SaveResult('CoolRunningFile', response.data)
+            self.SaveResult('CoolRunningFile.txt', response.data)
 
     def SaveResult(self, fileName, data):
-        path = os.path.join('Data', fileName)
+        path = os.path.join('E:\RunningData', fileName)
         with open(path, 'wb') as f:
             f.write(data)
+
+    def ExtractLinks(self, data):
+        pass
 
 coolRunningDownloader = Spider()
 coolRunningDownloader.DownloadFile('http://www.coolrunning.com/results/17/ct/Dec3_PearlH_set1.shtml')
---
+++ spiderTests.py
@@ -0,0 +1,4 @@
+from spider import Spider
+
+def test_SaveResults():
+    assert(1 == 1)
