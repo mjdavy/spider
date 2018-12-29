2a5d298d1642dd863e7c29a8b2b9522ea02f5280
mjdavy <mjdavy@hotmail.com>
2018-12-29 11:28:04 -0500

    Initial version

---
+++ spider.py
@@ -0,0 +1,19 @@
+import urllib3
+import os.path
+
+class Spider:
+    def DownloadFile(self, targetUrl):
+        http = urllib3.PoolManager()
+        response = http.request('GET', targetUrl)
+        if response.status == 200:
+            self.SaveResult('CoolRunningFile', response.data)
+
+    def SaveResult(self, fileName, data):
+        path = os.path.join('Data', fileName)
+        with open(path, 'wb') as f:
+            f.write(data)
+
+coolRunningDownloader = Spider()
+coolRunningDownloader.DownloadFile('http://www.coolrunning.com/results/17/ct/Dec3_PearlH_set1.shtml')
+
+
