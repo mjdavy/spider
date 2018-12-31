e252fda2b27f0daec5aecdd7254459915f48f8c8
Martin Davy <mjdavy@hotmail.com>
2018-12-31 15:17:03 -0500

    bug fixes

--- test_spider.py
+++ test_spider.py
@@ -1,5 +1,7 @@
 import pytest
 from spider import Spider
+import states
+import url_base
 import os
 
 testFile="foobar.txt"
@@ -20,3 +22,10 @@ def test_build_output_path():
     s = Spider()
     path = s.build_output_path(runningDataStore,base_url)
     assert(path == '\\\\monster.local\\RunningData\\Dec3_PearlH_set1.shtml')
+
+def test_states():
+    assert(len(states.states) == 51)
+
+def test_base_url_list():
+    base_urls = url_base.get_base_urls()
+    assert(len(base_urls) == 51*19)
--- url_base.py
+++ url_base.py
@@ -1,17 +1,15 @@
 from datetime import datetime
 from states import states
 
-# format is http://www.coolrunning.com/results/15/ma.shtml
+# format is http://www.coolrunning.com/results/<2 digit year>/<2 letter state>.shtml
+# example: race result for vermont in 2016 would be http://www.coolrunning.com/results/16/vt.shtml
 
-years = [str(y)[-2:] for y in range(2000,datetime.now().year+1)]
-print(years)
+def get_base_urls():
+    base = "www.coolrunning.com/results/"
+    years = [str(y)[-2:] for y in range(2000,datetime.now().year+1)]
+    return [base+year+"/"+state.lower()+".shtml" for state in states for year in years]
 
-base = "www.coolrunning.com/results/"
-base_urls = []
 
-for year in years:
-    allStates = [base+year+"/"+state.lower()+".shtml" for state in states]
-    base_urls.append(allStates)
-
-print(base_urls)
-print(len(base_urls))
+x = get_base_urls()
+print(x)
+print(len(x))
