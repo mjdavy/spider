5e01da0382b7f843e7638d9f86ed90423c9b3ff6
mjdavy <mjdavy@hotmail.com>
2019-01-01 16:32:51 -0500

    link extraction

--- spider.py
+++ spider.py
@@ -3,16 +3,21 @@ import os.path
 from bs4 import BeautifulSoup as bs
 
 class Spider:
-    def download_file(self, targetUrl, outputFolder):
+    def get_web_content(self, targetUrl):
         http = urllib3.PoolManager()
         response = http.request('GET', targetUrl)
+        htmlContent = ""
         if response.status == 200:
-            path = self.build_output_path(outputFolder, targetUrl)
-            self.save_result(path, response.data)
+            htmlContent = response.data
         else:
             print("error accessing url")
-               
+        return htmlContent
 
+    def download_file(self, targetUrl, outputFolder):
+        content = self.get_web_content(targetUrl)
+        path = self.build_output_path(outputFolder, targetUrl)
+        self.save_result(path, content)
+          
     def build_output_path(self, folder, url):
         name = url.split('/')[-1:]
         path = os.path.join(folder, name[0])
--- test_spider.py
+++ test_spider.py
@@ -1,5 +1,6 @@
 import pytest
 from spider import Spider
+from datetime import datetime
 import states
 import url_base
 import os
@@ -26,11 +27,16 @@ def test_build_output_path():
 def test_states():
     assert(len(states.states) == 51)
 
-from datetime import datetime
 def test_base_url_list():
     years = datetime.now().year - 2000 + 1
     base_urls = url_base.get_base_urls()
     assert(len(base_urls) == 51*years)
 
 def test_extract_links():
-    assert(1 == 1)
+    s = Spider()
+    url = 'http://www.coolrunning.com/results/17/pa.shtml'
+    html = s.get_web_content(url)
+    links = s.extract_links(html)
+    assert(len(links) > 0)
+
+
